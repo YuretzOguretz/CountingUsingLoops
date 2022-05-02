b602353004062ecad5cf54b62c578034fa654c33
YuretzOguretz <99295219+YuretzOguretz@users.noreply.github.com>
2022-05-02 18:56:20 -0400

    Change the header

--- src/countingusingloops/CountingUsingLoops.java
+++ src/countingusingloops/CountingUsingLoops.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Yura Nguyen
+ * May 2,2022
+ * Counting Loop Program
+ * Counts exponents using loops
  */
 package countingusingloops;
 import java.util.Scanner;
